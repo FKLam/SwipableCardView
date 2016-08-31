b7016393ea39a539adf9a088d896ea5ac3e7be28
Marc Nieto <richardhuling@marcbook.local>
2016-08-31 12:25:59 -0700

    Initial Commit

---
+++ SwipableCardView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SwipableCardView
+//
+//  Created by Marc Nieto on 8/31/16.
+//  Copyright © 2016 KandidProductions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
