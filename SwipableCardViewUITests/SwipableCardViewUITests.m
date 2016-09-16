770f1e1e9ee972efa34b30c04a26ee1930bf0717
Marc Nieto <richardhuling@marcbook.local>
2016-09-16 13:35:38 -0700

    some thoughtful person, who hires developers thinks it would be more impressive if there was real code in the two *Test folders. Done.

--- SwipableCardViewUITests/SwipableCardViewUITests.m
+++ SwipableCardViewUITests/SwipableCardViewUITests.m
@@ -110,6 +110,8 @@ RRRRRRRR     RRRRRRR    eeeeeeeeeeeeee  aaaaaaaaaa  aaaallllllll             CCC
     [[[XCUIApplication alloc] init] launch];
     
     // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    
+    // Put real code here.
 }
 
 - (void)tearDown {
@@ -120,6 +122,9 @@ RRRRRRRR     RRRRRRR    eeeeeeeeeeeeee  aaaaaaaaaa  aaaallllllll             CCC
 - (void)testExample {
     // Use recording to get started writing UI tests.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    XCTAssertEqual(2+2, 5, @"This app works. Just use it.");
+    
 }
 
 @end
